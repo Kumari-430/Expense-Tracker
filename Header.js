173cae3faa958efc0aaa16261d079e24198fbdea
Kumari Karthi <124483387+Kumari-430@users.noreply.github.com>
2023-09-29 12:35:12 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2 style={{color:"darkblue",textAlign:"center",fontSize:"48px"}}>
+            Expense Tracker
+        </h2>
+    )
+}
